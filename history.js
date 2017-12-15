2c34e050b380b988de6648a0cc76dd74e8df4e99
Andrew Chaa <andrew.chaa@just-eat.com>
2017-12-15 10:02:47 +0000

    change to EUR

--- history.js
+++ history.js
@@ -2,17 +2,17 @@ const fs = require('fs')
 const Gdax = require('gdax')
 const client = require('./dataClient')
 
-client.getHistoricRates('BTC-USD', (csv) => {
+client.getHistoricRates('BTC-EUR', (csv) => {
   console.log('BTC ...')
   fs.writeFileSync('./data/bst.csv', csv)
 })
 
-client.getHistoricRates('ETH-USD', (csv) => {
+client.getHistoricRates('ETH-EUR', (csv) => {
   console.log('ETH ...')
   fs.writeFileSync('./data/eth.csv', csv)
 })
 
-client.getHistoricRates('LTC-USD', (csv) => {
+client.getHistoricRates('LTC-EUR', (csv) => {
   console.log('LTC ...')
   fs.writeFileSync('./data/ltc.csv', csv)
 })
