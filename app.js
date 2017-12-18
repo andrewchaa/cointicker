94e0008c371a3dfc05348c05f82dad103174d7ea
Andrew Chaa <andrew.chaa@just-eat.com>
2017-12-18 12:26:29 +0000

    fix for route ltc

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/btc', btc);
 app.use('/eth', eth);
-app.use('/ltc', eth);
+app.use('/ltc', ltc);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
